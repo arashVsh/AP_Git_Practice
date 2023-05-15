5eeaeb2acdbdb65a13d03b4350f98de4ce556fa4
sepehr <mohammadsepehr.bazargan@gmail.com>
2023-05-15 17:33:49 +0330

    fixed 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -8,19 +8,18 @@ public:
 	float* p;
 	container(int s) :size(s){}
 	const int& getsize() { return size;}
-
 };
 
 class vector :public container {
 
 	int call_num;
 public:
-	explicit vector(int l) :len(l),size(1 * 100){
+	explicit vector(int l) :len(l),container(1 * 100){
 		p = new float();
 	}
 	int len;
-	int& getlen() const {
-		call_num ++;
+	int& getlen() {
+		call_num++;
 		return len;
 	}
 	~vector() = default;
@@ -32,9 +31,9 @@ int main() {
 	vector v1 = c1;
 	container& r1 = v1;
 	container c2 = 100;
-	c2.getsize() = 20;
+	c2.getsize();
 	cout << c2.getsize();
-	vector v2 = 100;
-	v2.getlen = 40;
+	vector v2(100);
+	v2.getlen() = 40;
 	cout << v2.getlen();
 }
