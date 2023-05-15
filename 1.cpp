11d5fa0813364183a45c07543f45346438e0502c
mhrg777 <mohamadramezanghanbari@gmail.com>
2023-05-15 18:19:03 +0330

    there was 8 error that was fixed in this

--- 1.cpp
+++ 1.cpp
@@ -2,39 +2,41 @@
 using namespace std;
 
 class container {
-
-	int size;
+    int size;
 public:
-	float* p;
-	container(int s) :size(s){}
-	const int& getsize() { return size;}
-
+    container(int s) : size(s) {}
+    const int& getsize() { return size; }
 };
 
-class vector :public container {
-
-	int call_num;
+class MyVector : public container {
+    int call_num;
+    int len;
 public:
-	explicit vector(int l) :len(l),size(1 * 100){
-		p = new float();
-	}
-	int len;
-	int& getlen() const {
-		call_num ++;
-		return len;
-	}
-	~vector() = default;
+    explicit MyVector(int l) : container(1 * 100), len(l), call_num(0) {
+       
+        p = new float();
+    }
+
+    int& getlen() {
+        call_num++;
+        return len;
+    }
+
+    ~MyVector() = default;
+    float* p; 
 };
 
 int main() {
+    container c1(100);
+    MyVector v1(100); 
+    container& r1 = v1;
+    container c2(100);
+    c2.getsize() = 20; 
+    cout << c2.getsize() << endl;
 
-	container c1(100);
-	vector v1 = c1;
-	container& r1 = v1;
-	container c2 = 100;
-	c2.getsize() = 20;
-	cout << c2.getsize();
-	vector v2 = 100;
-	v2.getlen = 40;
-	cout << v2.getlen();
+    MyVector v2(100); 
+    v2.getlen() = 40; 
+    cout << v2.getlen() << endl;
+
+    return 0;
 }
