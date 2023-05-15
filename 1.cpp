58e1ac175db35233e6163fcf705834b812192a2b
ramin <ramin.lgh.82@gmail.com>
2023-05-15 18:44:25 +0330

    file1 change

--- 1.cpp
+++ 1.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 
 class container {
-
+protected:
 	int size;
 public:
 	float* p;
-	container(int s) :size(s){}
-	const int& getsize() { return size;}
+	container(int s) :size(s) {}
+	int& getsize() { return size; } ///non const
 
 };
 
@@ -15,26 +15,29 @@ class vector :public container {
 
 	int call_num;
 public:
-	explicit vector(int l) :len(l),size(1 * 100){
+	explicit vector(int l) :len(l), container((1 * 100)) {  ///say cstr container
 		p = new float();
 	}
 	int len;
-	int& getlen() const {
-		call_num ++;
+	int& getlen() { /// non const
+		call_num++;
 		return len;
 	}
+
+
+
 	~vector() = default;
 };
 
 int main() {
 
 	container c1(100);
-	vector v1 = c1;
+	vector& v1 = (vector&)c1; ///static cast
 	container& r1 = v1;
 	container c2 = 100;
 	c2.getsize() = 20;
 	cout << c2.getsize();
-	vector v2 = 100;
-	v2.getlen = 40;
+	vector v2 = (vector)100; /// exlisit
+	v2.getlen() = 40; /// add ( ) function call
 	cout << v2.getlen();
 }
